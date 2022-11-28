059b784d876b0d32886b0eb7133b6bd2b4db82d5
Will2781 <102044577+Will2781@users.noreply.github.com>
2022-11-28 09:08:31 +0000

    Setting Framerate to 60
    disabled gameobject at the moment on main menu, It will carry on to all the levels as it wont destroy on load. Will enable the gameobject if the game runs slowly on xbox.

---
+++ Assets/Script/Will/LimitFramerate.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LimitFramerate : MonoBehaviour
+{
+    public int TargetFramerate = 60;
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = TargetFramerate;
+    }
+}
