b08bf03a0c5cc0d057a2fee24e8aec8bb7bf08a1
lKryptedl <kryptedgaming96@gmail.com>
2023-01-04 14:53:29 +0000

    Audio Testing

---
+++ Assets/Script/Jordan/New Folder/suck.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class suck : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public void onSuck()
+    {
+        audioSource.Play();
+    }
+}
