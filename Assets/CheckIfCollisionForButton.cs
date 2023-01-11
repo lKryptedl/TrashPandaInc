a6f1f93ca2b1d42a316606547bb9d3c5f238fbeb
Will2781 <102044577+Will2781@users.noreply.github.com>
2023-01-11 19:49:31 +0000

    Fixed Elevator

--- Assets/CheckIfCollisionForButton.cs
+++ Assets/CheckIfCollisionForButton.cs
@@ -5,7 +5,8 @@ using UnityEngine.InputSystem;
 
 public class CheckIfCollisionForButton : MonoBehaviour
 {
-    public Transform player;
+    public Transform player, Button;
+    public float angle;
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
@@ -13,15 +14,21 @@ public class CheckIfCollisionForButton : MonoBehaviour
             if (!Elevator.PlatformIsMovingDown && !Elevator.PlatformIsMovingUp)
             {
                 Gamepad controller = Gamepad.current;
-                //var Target = player.position - gameObject.transform.position;
-                //print(Vector3.Dot(transform.forward, Target));
-               // if (Vector3.Dot(transform.forward, Target) < 0)
-              //  {
+                /*var Target = (Button.position - player.position).normalized;
+                Target.y = 0;
+                print(Vector3.Dot(transform.forward, Target));*/
+                print(Vector3.Angle(player.transform.forward, Button.position - player.transform.position));
+                if (Vector3.Angle(player.transform.forward, Button.position - player.transform.position) < angle)
+                {
                     if (controller.yButton.isPressed)
                     {
                         print("Hit");
                         PlayerController.ButtonHit = true;
                     }
+                }
+                // if (Vector3.Dot(transform.forward, Target) < 0)
+                //  {
+                
               //  }
             }
         }
